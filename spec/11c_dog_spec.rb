b4ae94c676dacb2ad01dff53afc38e9ea9a18e61
CodeByBlazej <codebyblazej@gmail.com>
2025-01-19 18:56:30 +0000

    solves exercise

--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
-
+  subject(:jack) { described_class.new('Jack', nil, 'black')}
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'when method name is the same in multiple classes' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(jack.barking?).to be false
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(jack.sleeping?).to be true
   end
 end
